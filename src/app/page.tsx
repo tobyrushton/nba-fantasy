9a5639aa58cc06bf981c00dd62aa4756783b25eb
tobyrushton <tobyjrushton@icloud.com>
2023-11-15 16:55:54 +0000

    teams reorganised by conference and division

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,30 +10,93 @@ const getData = async (): Promise<team.ITeam[]> => {
     return data.data
 }
 
+type TDivisionNames =
+    | 'Atlantic'
+    | 'Central'
+    | 'Southeast'
+    | 'Northwest'
+    | 'Pacific'
+    | 'Southwest'
+interface IDivision {
+    name: TDivisionNames
+    teams: team.ITeam[]
+}
+
+interface IConference {
+    name: 'Eastern' | 'Western'
+    divisions: IDivision[]
+}
+
+const findByDivision = (
+    teams: team.ITeam[],
+    division: TDivisionNames
+): team.ITeam[] => {
+    return teams.filter(team => team.division === division)
+}
+
+const splitTeamsIntoConferences = (teams: team.ITeam[]): IConference[] => {
+    const conferences: IConference[] = [
+        {
+            name: 'Eastern',
+            divisions: [
+                { name: 'Atlantic', teams: findByDivision(teams, 'Atlantic') },
+                { name: 'Central', teams: findByDivision(teams, 'Central') },
+                {
+                    name: 'Southeast',
+                    teams: findByDivision(teams, 'Southeast'),
+                },
+            ],
+        },
+        {
+            name: 'Western',
+            divisions: [
+                {
+                    name: 'Northwest',
+                    teams: findByDivision(teams, 'Northwest'),
+                },
+                { name: 'Pacific', teams: findByDivision(teams, 'Pacific') },
+                {
+                    name: 'Southwest',
+                    teams: findByDivision(teams, 'Southwest'),
+                },
+            ],
+        },
+    ]
+
+    return conferences
+}
+
 const Home: FC = async () => {
     const teams = await getData()
-    const splitTeams: team.ITeam[][] = []
-    // splits into 2d array of 3 teams
-    while (teams.length) splitTeams.push(teams.splice(0, 3))
+    const splitTeams = splitTeamsIntoConferences(teams)
 
     return (
         <main>
             <div className={styles.wrapper}>
                 <h2>Search By Team</h2>
-                <div className={styles.teamList}>
-                    {splitTeams.map((teamList, i) => (
-                        // disabled as no other unique key is available
-                        // eslint-disable-next-line react/no-array-index-key
-                        <div className={styles.teamRow} key={i}>
-                            {teamList.map(team => (
-                                <Link
-                                    className={styles.team}
-                                    href={`/team/${team.id}`}
-                                    key={team.id}
+                <div className={styles.teamsContainer}>
+                    {splitTeams.map(conference => (
+                        <div
+                            className={styles.conferenceContainer}
+                            key={conference.name}
+                        >
+                            <h3>{conference.name} Conference</h3>
+                            {conference.divisions.map(division => (
+                                <div
+                                    className={styles.division}
+                                    key={division.name}
                                 >
-                                    <h3>{team.full_name}</h3>
-                                    <p>{team.abbreviation}</p>
-                                </Link>
+                                    <h4>{division.name} Division</h4>
+                                    <ul>
+                                        {division.teams.map(team => (
+                                            <li key={team.id}>
+                                                <Link href={`/team/${team.id}`}>
+                                                    {team.full_name}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
                             ))}
                         </div>
                     ))}
